e8cc50557a778c1aba5a22383c0cfb849f712c94
jisunkim <jenny.kim22856@gmail.com>
2025-05-15 18:23:01 +0900

    250515
    메뉴바
    - 메가메뉴 구현(BUT... 디테일은 아직..;;)

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,20 @@ $(window).on('scroll', function () {
     lastScroll = scrollTop;
 });
 
+$(function () {
+    $('body').addClass('js');
 
+    var $hamburger = $('.hamburger'),
+       $nav = $('#site-nav'),
+       $masthead = $('#masthead');
+
+    $hamburger.click(function () {
+       $(this).toggleClass('is-active');
+       $nav.toggleClass('is-active');
+       $masthead.toggleClass('is-active');
+       return false;
+    })
+ });
 
 // === floating menu ===
 const kakao = document.querySelector('.kakaotalk-btn');
